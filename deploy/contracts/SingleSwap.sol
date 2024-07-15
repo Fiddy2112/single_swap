a8a59dad2df42cb2c58bda3ed7bf9084de76587f
Fiddy <thnhlong2112@gmail.com>
2024-07-15 10:29:59 +0700

    fix & add interface IERC20

--- deploy/contracts/SingleSwap.sol
+++ deploy/contracts/SingleSwap.sol
@@ -2,11 +2,17 @@
 pragma solidity >=0.8.0 <0.9.0;
 pragma abicoder v2;
 
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+// import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
+interface IERC20{
+   function balanceOf(address account) external view returns (uint256);
+   function transfer(address to, uint256 value) external returns (bool);
+   function approve(address spender, uint256 value) external returns (bool);
+}
+
 contract SingleSwap {
-    // For the scope of these swap examples,
+// For the scope of these swap examples,
     // we will detail the design considerations when using
     // `exactInput`, `exactInputSingle`, `exactOutput`, and  `exactOutputSingle`.
 
@@ -17,10 +23,17 @@ contract SingleSwap {
 
     // This example swaps DAI/WETH9 for single path swaps and DAI/USDC/WETH9 for multi path swaps.
 
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    // uniswap weth
-    address public constant WETH = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant ZEPPLE = 0xC66386481Bf806BD0A9117EC4CfE6aab663BF035;
+
+    address public constant FLOPPY = 0x827c6ACfBc14A00E4D4ba274A6Feb0B59759C912;
+
+    // address public constant DAI = 0x68194a729C2450ad26072b3D33ADaCbcef39D574;
+    // Compatible with uniswap
+    // address public constant WETH = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
+
+    IERC20 public zeppleToken = IERC20(ZEPPLE);
+
+    IERC20 public floppyToken = IERC20(FLOPPY);
 
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
@@ -38,19 +51,19 @@ contract SingleSwap {
         // msg.sender must approve this contract
 
         // Transfer the specified amount of DAI to this contract.
-        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
+        // TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
 
         // Approve the router to spend DAI.
-        TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
+        zeppleToken.approve(address(swapRouter), amountIn);
 
         // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
         // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
         ISwapRouter.ExactInputSingleParams memory params =
             ISwapRouter.ExactInputSingleParams({
-                tokenIn: DAI,
-                tokenOut: WETH,
+                tokenIn: ZEPPLE,
+                tokenOut: FLOPPY,
                 fee: poolFee,
-                recipient: msg.sender,
+                recipient: address(this),
                 deadline: block.timestamp,
                 amountIn: amountIn,
                 amountOutMinimum: 0,
@@ -69,18 +82,18 @@ contract SingleSwap {
     /// @return amountIn The amount of DAI actually spent in the swap.
     function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum) external returns (uint256 amountIn) {
         // Transfer the specified amount of DAI to this contract.
-        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountInMaximum);
+        // TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountInMaximum);
 
         // Approve the router to spend the specifed `amountInMaximum` of DAI.
         // In production, you should choose the maximum amount to spend based on oracles or other data sources to acheive a better swap.
-        TransferHelper.safeApprove(DAI, address(swapRouter), amountInMaximum);
+        zeppleToken.approve(address(swapRouter), amountIn);
 
         ISwapRouter.ExactOutputSingleParams memory params =
             ISwapRouter.ExactOutputSingleParams({
-                tokenIn: DAI,
-                tokenOut: WETH,
+                tokenIn: ZEPPLE,
+                tokenOut: FLOPPY,
                 fee: poolFee,
-                recipient: msg.sender,
+                recipient: address(this),
                 deadline: block.timestamp,
                 amountOut: amountOut,
                 amountInMaximum: amountInMaximum,
@@ -93,9 +106,9 @@ contract SingleSwap {
         // For exact output swaps, the amountInMaximum may not have all been spent.
         // If the actual amount spent (amountIn) is less than the specified maximum amount, we must refund the msg.sender and approve the swapRouter to spend 0.
         if (amountIn < amountInMaximum) {
-            TransferHelper.safeApprove(DAI, address(swapRouter), 0);
-            TransferHelper.safeTransfer(DAI, msg.sender, amountInMaximum - amountIn);
+            zeppleToken.approve(address(swapRouter), 0);
+            zeppleToken.transfer( address(this), amountInMaximum - amountIn);
         }
     }
-
 }
+
